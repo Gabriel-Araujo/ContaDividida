e0406e38af5cebb0d67d9a19d5d8b971007e8bf2
Gabriel <gabrielsc.dev@gmail.com>
2022-08-27 21:21:55 -0300

    Adição de Funcionalidades (Lista de Usuários).

--- Prova de Conceito/scripts/FirstPage.js
+++ Prova de Conceito/scripts/FirstPage.js
@@ -1,25 +1,74 @@
-const SIDEMENU = document.querySelector(".sideMenu");
-const BACK_BUTTON = document.getElementById("back-button")
-const MENU_BUTTON = document.getElementById("menu-button")
-const LIST_ELEMENTS = document.querySelectorAll(".elemento-lista")
+const SIDE_MENU = document.querySelector(".sideMenu")
+const USER_MENU = document.querySelector(".usersMenu")
 
-BACK_BUTTON.addEventListener("click", fadeInOut)
-MENU_BUTTON.addEventListener("click", fadeInOut)
+const SIDE_MENU_BUTTON = document.getElementById("menu-button")
+const USERS_MENU_BUTTON = document.getElementById("user-list-button")
+const BACK_BUTTON = document.querySelectorAll(".backButton")
 
-LIST_ELEMENTS.forEach((elemento) => {
+const PRODUCT_LIST_ELEMENTS = document.querySelectorAll("#product-list .listElement")
+const USER_LIST_ELEMENTS = document.querySelectorAll("#user-list .listElement")
+
+BACK_BUTTON.forEach((element) => {
+    element.addEventListener("click", fadeInOut)
+})
+
+SIDE_MENU_BUTTON.addEventListener("click", fadeInOut)
+
+USERS_MENU_BUTTON.addEventListener("click", fadeInOut)
+
+PRODUCT_LIST_ELEMENTS.forEach((elemento) => {
     elemento.addEventListener("click", goToProduct);
 })
 
+USER_LIST_ELEMENTS.forEach((elemento) => {
+    elemento.addEventListener("click", goToUser)
+})
 
-function fadeInOut() {
-    if (SIDEMENU.classList.contains("hidden")) {
-        SIDEMENU.classList.remove("hidden")
+function fadeInOut(event) {
+    let component = event.target.parentElement.parentElement;
+
+    if (event.target.classList.contains("backButton")) {
+
+        if (component.classList.contains("sideMenu")) {
+        component.classList.add("hidden");
+        component.classList.add("l");
+        }
+
+        if (component.classList.contains("usersMenu")) {
+            component.classList.add("hidden");
+            component.classList.add("r");
+        }
     }
-    else {
-        SIDEMENU.classList.add("hidden")
+
+
+    if (event.target.id.toString() === "menu-button") {
+        showSideMenu();
+    }
+
+    if (event.target.id.toString() === "user-list-button") {
+        console.log(event.target.id)
+        showUserMenu();
+    }
+}
+
+function showSideMenu() {
+    if (SIDE_MENU.classList.contains("hidden")) {
+        SIDE_MENU.classList.remove("hidden")
+        SIDE_MENU.classList.remove("l")
+    }
+}
+
+function showUserMenu() {
+    if (USER_MENU.classList.contains("hidden")) {
+        USER_MENU.classList.remove("hidden")
+        USER_MENU.classList.remove("r")
     }
 }
 
 function goToProduct(event) {
     window.location = "../Pages/ProductDetails.html"
+}
+
+function goToUser(event) {
+    window.location = "../Pages/UserDetails.html"
 }
